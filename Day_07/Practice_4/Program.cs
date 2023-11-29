e2722f273f10433b2b3edab17c1dcaffd057058e
Mariam <m.dzamukashvili1@gmail.com>
2023-11-29 18:16:07 +0400

    Update practice_4

--- Day_07/Practice_4/Program.cs
+++ Day_07/Practice_4/Program.cs
@@ -19,20 +19,8 @@
 
         private static int CountNumberOfWords(string sentence)
         {
-            int count = 0;
-            string[] words = sentence.Split();
-            foreach (String word in words)
-            {
-                if (!char.IsPunctuation(word.ToCharArray()[0]))
-                {
-                    count++;
-                }
-                else if (word.Length > 1)
-                {
-                    count++;
-                }
-            }
-            return count;
+            string[] words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
         }
 
     }
