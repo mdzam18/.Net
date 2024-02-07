ba6d126718bbf2e35b2d9cae109d1629630e4e68
Mariam <m.dzamukashvili1@gmail.com>
2024-02-07 23:31:33 +0400

    Add Day_34 homework

---
+++ Day_34/CompanyManager.Application/Exceptions/CompanyNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyManagement.Application.Exceptions
+{
+    public  class CompanyNotFoundException : Exception
+    {
+        public string Code = "CompanyNotFound";
+
+        public CompanyNotFoundException(string message) : base(message) { }
+
+    }
+}
